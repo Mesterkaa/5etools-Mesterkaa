afb8052fb56c2ed5eaca4c85885d6c20968f1b7f
Mesterkaa <32128511+Mesterkaa@users.noreply.github.com>
2023-10-21 11:03:22 +0200

    Reworked .env file

--- custom/server/config/secrets.ts
+++ custom/server/config/secrets.ts
@@ -1,13 +1,17 @@
 import {logger} from "./logger.js";
 import * as dotenv from "dotenv";
-dotenv.config({ path: "./custom/server/.env"});
+dotenv.config({ path: ".env"});
 
-export const MONGODB_URI = process.env["MONGODB_URI"];
+const MONGODB_ADDRESS = process.env["MONGODB_ADDRESS"];
+const MONGODB_USER_ID = process.env["MONGODB_USER_ID"];
+const MONGODB_USER_SECRET = process.env["MONGODB_USER_SECRET"];
 
-if (!MONGODB_URI) {
-    logger.error("No mongo connection string. Set MONGODB_URI environment variable.");
+if (!MONGODB_ADDRESS || !MONGODB_USER_ID || !MONGODB_USER_SECRET) {
+    logger.error("One or more enviroment variables for MONGODB connection not set. Use: MONGODB_ADDRESS, MONGODB_USER_ID and MONGODB_USER_SECRET");
     process.exit(1);
 }
+
+export const MONGODB_URI = `mongodb://${MONGODB_USER_ID}:${MONGODB_USER_SECRET}@${MONGODB_ADDRESS}`
 
 export const PORT = Number(process.env["PORT"]);
 
